7e04ae9b4e94c87b35910b614102ee1cebd7307b
Osha Fuangkasae <ofuangka@gmail.com>
2017-08-22 20:30:14 -0500

    feat: implement tv server

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
  * PUT requests to /devices/:deviceId. /devices gets relayed as a device list request 
  * on the z-way network, and the device specific PUT request relays the request to the z-way server 
  * 
- * Usage: USERNAME=USERNAME PASSWORD=PASSWORD node index.js PORT
+ * Usage: USERNAME=USERNAME PASSWORD=PASSWORD node index.js PORT TV_SERVER_HOST TV_SERVER_PORT
  * 	where PORT is the port to listen on,
- *  USERNAME and PASSWORD are the credentials to use when relaying to the z-way server
+ *  USERNAME and PASSWORD are the credentials to use when relaying to the z-way server, 
+ *  and TV_SERVER_HOST TV_SERVER_PORT are the host and port are the TV server and port
  */
 
 /**
@@ -87,6 +88,15 @@ function post(path, postData, options) {
 	}
 
 	/**
+	 * Checks if the request is for TV
+	 * 
+	 * @param {object} request 
+	 */
+	function isTvRequest(request) {
+		return request.params.deviceId === TV.id || request.params.deviceId === TELEVISION.id;
+	}
+
+	/**
 	 * Attempts to make a GET request using an existing session ID. If the attempt 
 	 * fails with a 401 unauthorized, attempts to start a new session and then 
 	 * make the same GET request
@@ -106,16 +116,32 @@ function post(path, postData, options) {
 		}
 		return authGet(fullPath);
 	}
-	if (process.argv.length < 3) {
-		console.log(`Usage: USERNAME=USERNAME PASSWORD=PASSWORD node ${__filename} PORT`);
+	if (process.argv.length < 5) {
+		console.log(`Usage: USERNAME=USERNAME PASSWORD=PASSWORD node ${__filename} PORT TV_SERVER_HOST TV_SERVER_PORT`);
 		process.exit(-1);
 	}
 	const PORT = process.argv[2],
 		USERNAME = process.env.USERNAME,
 		PASSWORD = process.env.PASSWORD,
+		TV_SERVER_HOST = process.env.TV_SERVER_HOST,
+		TV_SERVER_POST = process.env.TV_SERVER_PORT,
 		ZWAY = '/ZAutomation/api/v1',
 		OPTIONS = {
 			port: 8083
+		},
+		TV = {
+			id: 'tv',
+			metrics: {
+				title: 'TV',
+				level: 'off'
+			}
+		},
+		TELEVISION = {
+			id: 'television',
+			metrics: {
+				title: 'Television',
+				level: 'off'
+			}
 		};
 
 	var express = require('express'),
@@ -135,9 +161,11 @@ function post(path, postData, options) {
 		zwayGet('/devices')
 			.then(response => {
 				var devices = JSON.parse(response.responseText).data.devices;
-				inResponse.send(JSON.stringify(devices));
+				devices.push(TV);
+				devices.push(TELEVISION);
+				inResponse.status(200).send(JSON.stringify(devices));
 			})
-			.catch(error => inResponse.send(JSON.stringify({ error: error })));
+			.catch(error => inResponse.status(500).send(JSON.stringify({ error: error })));
 	});
 
 	/* express put handler */
@@ -146,10 +174,16 @@ function post(path, postData, options) {
 
 		var command = (inRequest.body.level === 'on') ? 'on' : 'off';
 
-		/* make a request to the z-way server */
-		zwayGet(`/devices/${inRequest.params.deviceId}/command/${command}`)
-			.then(response => inResponse.send(JSON.stringify({ code: 200, message: '200 OK' })))
-			.catch(error => inResponse.send(JSON.stringify({ error: error })));
+		if (isTvRequest(inRequest)) {
+			post('/', { host: TV_SERVER_HOST, port: TV_SERVER_POST })
+				.then(response => inResponse.status(200).send(JSON.stringify({ code: 200, message: '200 OK' })))
+				.catch(error => inResponse.status(500).send(JSON.stringify({ error: error })));
+		} else {
+			/* make a request to the z-way server */
+			zwayGet(`/devices/${inRequest.params.deviceId}/command/${command}`)
+				.then(response => inResponse.status(200).send(JSON.stringify({ code: 200, message: '200 OK' })))
+				.catch(error => inResponse.status(500).send(JSON.stringify({ error: error })));
+		}
 	});
 
 	http.createServer(server).listen(PORT);
