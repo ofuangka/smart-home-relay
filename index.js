4e5934b9e53967ad17aad2544509571becffdb83
Osha Fuangkasae <ofuangka@gmail.com>
2017-08-24 21:59:26 -0500

    feat: initial skeleton for changeChannel

--- index.js
+++ index.js
@@ -8,6 +8,44 @@
  *  USERNAME and PASSWORD are the credentials to use when relaying to the z-way server, 
  *  and TV_SERVER_HOST TV_SERVER_PORT are the host and port are the TV server and port
  */
+if (process.argv.length < 5) {
+	console.log(`Usage: USERNAME=USERNAME PASSWORD=PASSWORD node ${__filename} PORT TV_SERVER_HOST TV_SERVER_PORT`);
+	process.exit(-1);
+}
+
+const ZWAY = '/ZAutomation/api/v1',
+	OPTIONS = {
+		port: 8083
+	},
+	TV = {
+		id: 'tv',
+		deviceType: 'television',
+		metrics: {
+			title: 'TV'
+		}
+	},
+	TELEVISION = {
+		id: 'television',
+		deviceType: 'television',
+		metrics: {
+			title: 'Television'
+		}
+	},
+	ROKU = {
+		id: 'roku',
+		deviceType: 'roku',
+		metrics: {
+			title: 'Roku'
+		}
+	};
+
+var server = express(),
+	port = process.argv[2],
+	username = process.env.USERNAME,
+	password = process.env.PASSWORD,
+	tvServerHost = process.argv[3],
+	tvServerPort = process.argv[4],
+	sid;
 
 /**
  * cheap polyfill for Object.assign()
@@ -54,160 +92,145 @@ function post(path, postData, options) {
 	return httpPromise(assign({ method: 'POST', path: path }, options), postData);
 }
 
+function handleRokuPowerRequest(inRequest, inResponse) {
+	inResponse.status(500).send(JSON.stringify({ error: 'Not yet implemented' }));
+}
+
+function handleTvChangeChannelRequest(inRequest, inResponse) {
+	console.log(`TvChangeChannelRequest: ${inRequest}`);
+	inResponse.status(500).send(JSON.stringify({ error: 'Not yet implemented' }));
+}
+
+function handleRokuChangeChannelRequest(inRequest, inResponse) {
+	console.log(`RokuChangeChannelRequest: ${inRequest}`);
+	inResponse.status(500).send(JSON.stringify({ error: 'Not yet implemented' }));
+}
+
 var express = require('express'),
 	bodyParser = require('body-parser'),
 	http = require('http');
 
-(function main() {
+/**
+ * Gets a z-way session ID using the USERNAME and PASSWORD
+ */
+function getSession() {
+	var fullPath = `${ZWAY}/login`;
+	return post(fullPath, JSON.stringify({ login: username, password: password }), getOptions())
+		.then(response => JSON.parse(response.responseText).data.sid);
+}
 
-	/**
-	 * Gets a z-way session ID using the USERNAME and PASSWORD
-	 */
-	function getSession() {
-		var fullPath = `${ZWAY}/login`;
-		return post(fullPath, JSON.stringify({ login: USERNAME, password: PASSWORD }), getOptions())
-			.then(response => JSON.parse(response.responseText).data.sid);
+/**
+ * Returns options for http
+ * 
+ * @param {boolean} includeSid Whether to include the sid 
+ */
+function getOptions(includeSid) {
+	return includeSid ? assign({ headers: { ZWAYSession: sid } }, OPTIONS) : OPTIONS;
+}
+
+/**
+ * Opens a session and uses that session to make a GET request
+ * 
+ * @param {string} path The request path 
+ */
+function authGet(path) {
+	return getSession()
+		.then(_sid => sid = _sid)
+		.then(() => get(path, getOptions(true)));
+}
+
+/**
+ * Checks if the request is for TV
+ * 
+ * @param {object} request 
+ */
+function isTvRequest(request) {
+	return request.params.deviceId === TV.id || request.params.deviceId === TELEVISION.id;
+}
+
+function isRokuRequest(request) {
+	return request.params.deviceId === ROKU.id;
+}
+
+function handleTvPowerRequest(inRequest, inResponse) {
+	post('/', command, { host: tvServerHost, port: tvServerPort })
+		.then(response => inResponse.status(200).send(JSON.stringify({ code: 200, message: '200 OK' })))
+		.catch(error => inResponse.status(500).send(JSON.stringify({ error: error })));
+}
+
+/**
+ * Attempts to make a GET request using an existing session ID. If the attempt 
+ * fails with a 401 unauthorized, attempts to start a new session and then 
+ * make the same GET request
+ * 
+ * @param {string} path The request path 
+ */
+function zwayGet(path) {
+	var fullPath = `${ZWAY}${path}`;
+	if (sid) {
+		return get(fullPath, getOptions(true))
+			.then(response => {
+				if (response.statusCode === 401) {
+					return authGet(fullPath);
+				}
+				return response;
+			});
 	}
+	return authGet(fullPath);
+}
 
-	/**
-	 * Returns options for http
-	 * 
-	 * @param {boolean} includeSid Whether to include the sid 
-	 */
-	function getOptions(includeSid) {
-		return includeSid ? assign({ headers: { ZWAYSession: sid } }, OPTIONS) : OPTIONS;
-	}
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 
-	/**
-	 * Opens a session and uses that session to make a GET request
-	 * 
-	 * @param {string} path The request path 
-	 */
-	function authGet(path) {
-		return getSession()
-			.then(_sid => sid = _sid)
-			.then(() => get(path, getOptions(true)));
-	}
+/* express get handler */
+server.get('/devices', function (inRequest, inResponse) {
+	console.log('GET request for /devices received');
 
-	/**
-	 * Checks if the request is for TV
-	 * 
-	 * @param {object} request 
-	 */
-	function isTvRequest(request) {
-		return request.params.deviceId === TV.id || request.params.deviceId === TELEVISION.id;
-	}
+	/* make a request to the z way server */
+	zwayGet('/devices')
+		.then(response => {
+			var devices = JSON.parse(response.responseText).data.devices;
+			devices.push(TV);
+			devices.push(TELEVISION);
+			devices.push(ROKU);
+			inResponse.status(200).send(JSON.stringify(devices));
+		})
+		.catch(error => inResponse.status(500).send(JSON.stringify({ error: error })));
+});
 
-	function isRokuRequest(request) {
-		return request.params.deviceId === ROKU.id;
-	}
+/* express put handler */
+server.put('/devices/:deviceId', (inRequest, inResponse) => {
+	console.log(`PUT request for /devices/${inRequest.params.deviceId} received`);
 
-	function handleTvRequest(inRequest, inResponse) {
-		post('/', command, { host: TV_SERVER_HOST, port: TV_SERVER_POST })
+	var command = (inRequest.body.level === 'on') ? 'on' : 'off';
+
+	if (isTvRequest(inRequest)) {
+		handleTvPowerRequest(inRequest, inResponse);
+	} else if (isRokuRequest(inRequest)) {
+		handleRokuPowerRequest(inRequest, inResponse);
+	} else {
+
+		/* make a request to the z-way server */
+		zwayGet(`/devices/${inRequest.params.deviceId}/command/${command}`)
 			.then(response => inResponse.status(200).send(JSON.stringify({ code: 200, message: '200 OK' })))
 			.catch(error => inResponse.status(500).send(JSON.stringify({ error: error })));
 	}
+});
 
-	function handleRokuRequest(inRequest, inResponse) {
-		inResponse.status(500).send(JSON.stringify({ error: 'To be implemented' }));
+server.put('/devices/:deviceId/channels', (inRequest, inResponse) => {
+	console.log(`PUT request for /devices/${inRequest.params.deviceId}/channels received`);
+
+	var channel = inRequest.body;
+
+	if (isTvRequest(inRequest)) {
+		handleTvChangeChannelRequest(inRequest, inResponse);
+	} else if (isRokuRequest(inRequest)) {
+		handleRokuChangeChannelRequest(inRequest, inResponse);
+	} else {
+		inResponse.status(500).send(JSON.stringify({ error: new Error(`Device ${inRequest.params.deviceId} does not support Alexa.ChannelController`) }));
 	}
 
-	/**
-	 * Attempts to make a GET request using an existing session ID. If the attempt 
-	 * fails with a 401 unauthorized, attempts to start a new session and then 
-	 * make the same GET request
-	 * 
-	 * @param {string} path The request path 
-	 */
-	function zwayGet(path) {
-		var fullPath = `${ZWAY}${path}`;
-		if (sid) {
-			return get(fullPath, getOptions(true))
-				.then(response => {
-					if (response.statusCode === 401) {
-						return authGet(fullPath);
-					}
-					return response;
-				});
-		}
-		return authGet(fullPath);
-	}
-	if (process.argv.length < 5) {
-		console.log(`Usage: USERNAME=USERNAME PASSWORD=PASSWORD node ${__filename} PORT TV_SERVER_HOST TV_SERVER_PORT`);
-		process.exit(-1);
-	}
-	const PORT = process.argv[2],
-		USERNAME = process.env.USERNAME,
-		PASSWORD = process.env.PASSWORD,
-		TV_SERVER_HOST = process.argv[3],
-		TV_SERVER_POST = process.argv[4],
-		ZWAY = '/ZAutomation/api/v1',
-		OPTIONS = {
-			port: 8083
-		},
-		TV = {
-			id: 'tv',
-			deviceType: 'television',
-			metrics: {
-				title: 'TV'
-			}
-		},
-		TELEVISION = {
-			id: 'television',
-			deviceType: 'television',
-			metrics: {
-				title: 'Television'
-			}
-		},
-		ROKU = {
-			id: 'roku',
-			deviceType: 'roku',
-			metrics: {
-				title: 'Roku'
-			}
-		};
+});
 
-	var server = express(),
-		sid;
-
-	server.use(bodyParser.json());
-	server.use(bodyParser.urlencoded({ extended: true }));
-
-	/* express get handler */
-	server.get('/devices', function (inRequest, inResponse) {
-		console.log('GET request for /devices received');
-
-		/* make a request to the z way server */
-		zwayGet('/devices')
-			.then(response => {
-				var devices = JSON.parse(response.responseText).data.devices;
-				devices.push(TV);
-				devices.push(TELEVISION);
-				devices.push(ROKU);
-				inResponse.status(200).send(JSON.stringify(devices));
-			})
-			.catch(error => inResponse.status(500).send(JSON.stringify({ error: error })));
-	});
-
-	/* express put handler */
-	server.put('/devices/:deviceId', (inRequest, inResponse) => {
-		console.log(`PUT request for /devices/${inRequest.params.deviceId} received`);
-
-		var command = (inRequest.body.level === 'on') ? 'on' : 'off';
-
-		if (isTvRequest(inRequest)) {
-			handleTvRequest(inRequest, inResponse);
-		} else if (isRokuRequest(inRequest)) {
-			handleRokuRequest(inRequest, inResponse);
-		} else {
-
-			/* make a request to the z-way server */
-			zwayGet(`/devices/${inRequest.params.deviceId}/command/${command}`)
-				.then(response => inResponse.status(200).send(JSON.stringify({ code: 200, message: '200 OK' })))
-				.catch(error => inResponse.status(500).send(JSON.stringify({ error: error })));
-		}
-	});
-
-	http.createServer(server).listen(PORT);
-	console.log(`Server listening on port ${PORT}`);
-})();
+http.createServer(server).listen(port);
+console.log(`Server listening on port ${port}`);
